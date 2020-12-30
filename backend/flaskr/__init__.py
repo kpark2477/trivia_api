1ad8494c0d997b424ead3820d417e84c24ecefb4
kyuwon <kpark2477@gmail.com>
2020-12-30 18:31:58 +0900

    add quiz endpoints and error handlers

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -149,64 +149,51 @@ def create_app(test_config=None):
         'currentCategory': formatted_categories
       })
 
-  '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
+  @app.route('/quizzes', methods=['POST'])
+  def retrieve_questions_for_quiz():
+      body = request.get_json()
+      previous_questions = body.get('previous_questions', None)
+      quiz_category = body.get('quiz_category', None)
 
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
+      quiz_category_id = quiz_category.get('id')
 
-  '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
+      if quiz_category_id == 0:
+          questions = Question.query.filter(~Question.id.in_(previous_questions)).all()
+      else:
+          questions = Question.query.filter(
+            Question.category == quiz_category_id).filter(~Question.id.in_(previous_questions)).all()
 
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
+      if len(questions) == 0:
+          abort(404)
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
+      try:
+          formatted_questions = [question.format() for question in questions]
+          random_number = random.randrange(0, len(formatted_questions))
+          random_question = formatted_questions[random_number]
 
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-  '''
+          return jsonify({
+            'question': random_question
+          })
 
-  '''
-  @TODO: 
-  Create a GET endpoint to get questions based on category. 
-
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
-  '''
-
-
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
-
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-  '''
-
-  '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
+      except:
+          abort(422)
   
+  @app.errorhandler(404)
+  def not_found(error):
+      return jsonify({
+          "success": False,
+          "error": 404,
+          "message": "Not found"
+          }), 404
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+      return jsonify({
+          "success": False,
+          "error": 422,
+          "message": "Unprocessable"
+          }), 422
+
   return app
 
     
